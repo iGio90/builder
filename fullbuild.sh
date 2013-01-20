52b49c06095795c95dbe45d4aefec567be9fb6ee
igio90 <giovanni.rocca.90@gmail.com>
2013-01-20 02:30:36 +0100

    derp 2

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,3 +1,3 @@
 DEVICE="$1"
 TARGET="$2"
-cd out/target/product/$DEVICE/ && mkdir tmp && cp jellybam-*.zip tmp/ && cd tmp && echo "Unzipping the build" && unzip -q jellybam-*.zip && rm *.zip && cp -r ../../../../../fullbuild/$TARGET/system/* system/ && echo "Zipping the final build" && zip -r -q jellybam-5.1.0_$DEVICE.STABLE.zip && echo "Moving the build into final folder" && mkdir ../../../../../final && mv jellybam-* ../../../../../final/ && cd ../ && rm -r tmp;
+cd out/target/product/$DEVICE/ && mkdir tmp && cp jellybam-*.zip tmp/ && cd tmp && echo "Unzipping the build" && unzip -q jellybam-*.zip && rm *.zip && cp -r ../../../../../fullbuild/$TARGET/system/* system/ && echo "Zipping the final build" && zip -r -q jellybam-5.1.0_$DEVICE.STABLE.zip * && echo "Moving the build into final folder" && mkdir ../../../../../final && mv jellybam-* ../../../../../final/ && cd ../ && rm -r tmp;
